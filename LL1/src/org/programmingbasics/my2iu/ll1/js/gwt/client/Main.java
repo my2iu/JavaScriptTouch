90f9703a9301cd18dab5f900d196bc3dc98473f3
Ming <git@programmingbasics.org>
2013-12-21 19:35:02 -0500

    Moving project to GWT so that I can try building a prototype UI.

---
+++ LL1/src/org/programmingbasics/my2iu/ll1/js/gwt/client/Main.java
@@ -0,0 +1,28 @@
+package org.programmingbasics.my2iu.ll1.js.gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+import elemental.client.Browser;
+import elemental.dom.Document;
+import elemental.dom.Element;
+import elemental.events.Event;
+import elemental.events.EventListener;
+import elemental.html.CanvasElement;
+import elemental.html.CanvasRenderingContext2D;
+import elemental.html.Window;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Main implements EntryPoint
+{
+  /**
+   * This is the entry point method.
+   */
+  public void onModuleLoad()
+  {
+//    Audio.initialize();
+//    Screen.switchTo(new TitleScreen());
+//    Screen.switchTo(new MainGameScreen(0));
+  }
+}
